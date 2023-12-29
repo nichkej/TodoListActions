9753fc8a6a213de3623f1e10241a1181ba4e4512
Nikola Jovicevic <nikola.jovicevic@vega.local>
2023-12-29 14:10:58 +0100

    Edit failing test.

--- TodoListActions.Test/TaskControllerTest.cs
+++ TodoListActions.Test/TaskControllerTest.cs
@@ -14,13 +14,13 @@
 
         // failing test
         [Fact]
-        public async Task GetById_InputIs1_ReturnsBadRequest()
+        public async Task GetById_InputIs1_ReturnsNotFound()
         {
             var factory = new TodoListActionsWebApplicationFactory();
             var client = factory.CreateClient();
 
             var response = await client.GetAsync("/api/Task/id=1");
-            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
